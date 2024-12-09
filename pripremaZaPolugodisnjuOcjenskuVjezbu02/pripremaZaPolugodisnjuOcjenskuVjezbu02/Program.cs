331489e3200e9a5485f87992ff87cb29e04a2688
DavidCuturic5 <david.cuturic5@gmail.com>
2024-12-09 12:52:42 +0100

    Add files via upload

---
+++ pripremaZaPolugodisnjuOcjenskuVjezbu02/pripremaZaPolugodisnjuOcjenskuVjezbu02/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pripremaZaPolugodisnjuOcjenskuVjezbu02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for(int i=1900; i<=2100; i++)
+            {
+                if((i%400 == 0)||(i%4==0 && i%100 != 100))
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
